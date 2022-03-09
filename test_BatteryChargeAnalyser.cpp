1e4016421e05e5ffbad123c28df1b53dd9e5883a
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-09 18:31:21 +0530

    Update test_BatteryChargeAnalyser.cpp
    Initial commit with blank passing test to make sure environment is working

--- test_BatteryChargeAnalyser.cpp
+++ test_BatteryChargeAnalyser.cpp
@@ -1,1 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "test/catch.hpp"
+#include "BatteryChargingAnalyser.h"
+
+TEST_CASE("Blank test") 
+{
+  REQUIRE(1 == 1);
+}
