840a45451c10e3049b4a94fcde92b1de72b1c35f
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-10 12:24:57 +0530

    Update BatteryChargeAnalyser.c

--- BatteryChargeAnalyser.c
+++ BatteryChargeAnalyser.c
@@ -1,6 +1,32 @@
 #include "BatteryChargeAnalyser.h"
 
+static char printchar[100];
+
+void getConsecutiveRangeFromArray(int* MinVal, int* MaxVal, unsigned int CurrentArray)
+{
+  *MinVal = 4;
+  *MaxVal = 6;
+  return;
+}
+
+unsigned int getNumberOfOccurencesFromRange(int MinVal, int MaxVal, unsigned int CurrentArray)
+{
+  return 3; 
+}
+
+void getstringFromValues(MinVal, MaxVal, NumOfOccurence)
+{
+  sprintf(printchar, "%d-%d, %d", MinVal, MaxVal, NumOfOccurence);
+}
+
 char* NumberOfReadingsInRange(unsigned int* CurrentArray, unsigned int SizeOfCurrentArray)
 {
-  return "4-5, 2";
+  int MinVal, MaxVal = 0;
+  unsigned int NumOfOccurence = 0;
+  
+  getConsecutiveRangeFromArray(&MinVal, &MaxVal, CurrentArray, SizeOfCurrentArray);
+  NumOfOccurence = getNumberOfOccurencesFromRange(MinVal, MaxVal, CurrentArray, SizeOfCurrentArray);
+  getstringFromValues(MinVal, MaxVal, NumOfOccurence);
+  
+  return printchar;
 }
