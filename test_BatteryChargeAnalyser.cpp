0700835ab5ff9e0de5aefbab59de89dc4c8a77d3
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-10 11:51:07 +0530

    Update test_BatteryChargeAnalyser.cpp

--- test_BatteryChargeAnalyser.cpp
+++ test_BatteryChargeAnalyser.cpp
@@ -1,11 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include <cstring.h>
 #include "BatteryChargeAnalyser.h"
 
 uint8 CurrentArray[100] = {4,5};
 
 TEST_CASE("To give 4,5 as input and expect 4-5, 2 as output") 
 {
-  REQUIRE(CurrentOccurenceInRange(CurrentArray, 2), "4-5, 2");
+  REQUIRE(strcmp(NumberOfReadingsInRange(uint8* CurrentArray, uint8 SizeOfCurrentArray)(CurrentArray, 2), "4-5, 2"));
 }
