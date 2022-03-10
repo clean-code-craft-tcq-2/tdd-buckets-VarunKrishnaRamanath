6c884301920db816b5ffe014604c877cfb2bcdcc
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-10 12:40:50 +0530

    Update test_BatteryChargeAnalyser.cpp

--- test_BatteryChargeAnalyser.cpp
+++ test_BatteryChargeAnalyser.cpp
@@ -5,10 +5,10 @@
 #include "BatteryChargeAnalyser.h"
 
 //TEST_CASE("To give 4,5 as input and expect 4-5, 2 as output") 
-{
+//{
   //unsigned int CurrentArray[] = {4,5};
   //REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, 2), "4-5, 2\n"));
-}
+//}
 
 TEST_CASE("To give 4,5,6 as input and expect 4-6, 3 as output") 
 {
