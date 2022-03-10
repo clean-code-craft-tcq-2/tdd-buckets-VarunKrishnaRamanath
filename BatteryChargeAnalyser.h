49bda08794022068cfb3df0dbb82940d8c9b2356
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-10 12:34:24 +0530

    Update BatteryChargeAnalyser.h

--- BatteryChargeAnalyser.h
+++ BatteryChargeAnalyser.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <string.h>
 
-void getConsecutiveRangeFromArray(int* MinVal, int* MaxVal, unsigned int CurrentArray);
-unsigned int getNumberOfOccurencesFromRange(int MinVal, int MaxVal, unsigned int CurrentArray);
-void getstringFromValues(MinVal, MaxVal, NumOfOccurence);
+void getConsecutiveRangeFromArray(int* MinVal, int* MaxVal, unsigned int* CurrentArray);
+unsigned int getNumberOfOccurencesFromRange(int MinVal, int MaxVal, unsigned int* CurrentArray);
+void getstringFromValues(int MinVal, int MaxVal, unsigned int NumOfOccurence);
 
 char* NumberOfReadingsInRange(unsigned int* CurrentArray, unsigned int SizeOfCurrentArray);
