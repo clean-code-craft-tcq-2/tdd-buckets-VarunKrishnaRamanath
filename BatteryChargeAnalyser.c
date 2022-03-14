3827465bfbc98bf3e782b5c0ec78676bd4550895
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-14 18:54:37 +0530

    Update BatteryChargeAnalyser.c

--- BatteryChargeAnalyser.c
+++ BatteryChargeAnalyser.c
@@ -2,10 +2,31 @@
 
 static char printchar[100];
 
+void getConsecutiveArray(unsigned int* ConsecutiveArray, unsigned int* SizeOfConsecutiveArray, unsigned int* CurrentArray, unsigned int SizeOfCurrentArray)
+{
+  ConsecutiveArray[0] = 4;
+  ConsecutiveArray[1] = 5;
+  ConsecutiveArray[2] = 6;
+ 
+  SizeOfConsecutiveArray = 3
+  
+  return; 
+}
+
 void getConsecutiveRangeFromArray(int* MinVal, int* MaxVal, unsigned int* CurrentArray, unsigned int SizeOfCurrentArray)
 {
-  *MinVal = 4;
-  *MaxVal = 6;
+ unsigned int count = 0;
+ unsigned int NumOfConsecutiveArray = 0;
+ unsigned int SizeOfConsecutiveArray = 0;
+ int TempArray[100];
+ 
+ memset(TempArray, 0, sizeof(TempArray));
+ 
+ getConsecutiveArray(TempArray, &SizeOfConsecutiveArray, CurrentArray, SizeOfCurrentArray);
+ //getMinMaxOfConsecutiveArray();
+ 
+  *MinVal = TempArray[0];
+  *MaxVal = TempArray[SizeOfConsecutiveArray-1];
   return;
 }
 
