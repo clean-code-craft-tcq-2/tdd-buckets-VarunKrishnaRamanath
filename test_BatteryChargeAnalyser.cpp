7564172c217d02f65c85d589da3704b46a58255c
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-15 15:46:08 +0530

    Update test_BatteryChargeAnalyser.cpp

--- test_BatteryChargeAnalyser.cpp
+++ test_BatteryChargeAnalyser.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include "BatteryChargeAnalyser.h"
 
+#define SIZEOFARRAY(a)  (sizeof(a)/sizeof(unsigned int))
+
 //TEST_CASE("To give 4,5 as input and expect 4-5, 2 as output") 
 //{
   //unsigned int CurrentArray[] = {4,5};
@@ -21,8 +23,8 @@ TEST_CASE("To give 4,5,6 as input and expect 4-6, 3 as output")
   REQUIRE(MaxVal == 6);
   
   unsigned int TempArray2[3] = {4, 5, 6};
-  REQUIRE(getMinOfGivenArray(TempArray2, 3) == 4);
-  REQUIRE(getMaxConsecutiveOfGivenArray(getMinOfGivenArray(TempArray2, 3), TempArray2, 3) == 6);
+  REQUIRE(getMinOfGivenArray(TempArray2, SIZEOFARRAY(TempArray2)) == 4);
+  REQUIRE(getMaxConsecutiveOfGivenArray(getMinOfGivenArray(TempArray2, SIZEOFARRAY(TempArray2)), TempArray2, SIZEOFARRAY(TempArray2)) == 6);
   
   REQUIRE(CheckWithinRange(5,4,6) == true);
 }
@@ -30,23 +32,23 @@ TEST_CASE("To give 4,5,6 as input and expect 4-6, 3 as output")
 TEST_CASE("To give 4,5,6,6,7 as input and expect 4-7, 5 as output") 
 {
   unsigned int CurrentArray[] = {4,5,6,6,7};
-  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, 5), "4-7, 5\n"));
+  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, SIZEOFARRAY(CurrentArray)), "4-7, 5\n"));
 }
 
 TEST_CASE("To give array of non consecutive numbers and expect error output")
 {
   unsigned int CurrentArray[] = {1,3,5,7,9};
-  REQUIRE(strstr(NumberOfReadingsInRange(CurrentArray, 5), "Error"));
+  REQUIRE(strstr(NumberOfReadingsInRange(CurrentArray, SIZEOFARRAY(CurrentArray)), "Error"));
 }
 
 TEST_CASE("To give 4,5,6,6,7,9 as input and expect 4-7, 5 as output") 
 {
   unsigned int CurrentArray[] = {4,5,6,6,7,9};
-  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, 5), "4-7, 5\n"));
+  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, SIZEOFARRAY(CurrentArray)), "4-7, 5\n"));
 }
 
 TEST_CASE("To give 4,5,6,6,8,9,10 as input and expect 4-6, 4 and 8-10, 3 as output") 
 {
   unsigned int CurrentArray[] = {4,5,6,6,8,9,10};
-  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, 5), "4-6, 4\n8-10,3\n"));
+  REQUIRE(strcmp(NumberOfReadingsInRange(CurrentArray, SIZEOFARRAY(CurrentArray)), "4-6, 4\n8-10,3\n"));
 }
