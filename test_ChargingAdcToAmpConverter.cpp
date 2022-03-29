3b7d2138b6f5c7ab98c6e481c3a7428b0db24c7f
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-29 20:36:50 +0530

    Update test_ChargingAdcToAmpConverter.cpp

--- test_ChargingAdcToAmpConverter.cpp
+++ test_ChargingAdcToAmpConverter.cpp
@@ -5,20 +5,26 @@
 #include "ChargingAdcToAmpConverter.h"
 
 unsigned int AmpValue = 0;
-TEST_CASE("To check if ADC o/p 0 is read as 0 amp by the converter")
+TEST_CASE("To check if ADC o/p 0 is read as 0A by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(0, &AmpValue) == 0); /* function returns 0 indicating no error */
+  REQUIRE(ConvertAdcToAmp(0, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
   REQUIRE(AmpValue == 0);
 }
 
-TEST_CASE("To check if ADC o/p 4095 is read as 10 amp by the converter")
+TEST_CASE("To check if ADC o/p 4094 is read as 10A by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(4095, &AmpValue) == 0); /* function returns 0 indicating no error */
+  REQUIRE(ConvertAdcToAmp(4094, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
   REQUIRE(AmpValue == 10);
 }
 
-TEST_CASE("To check if ADC o/p 4096 or greater is read as error by the converter")
+TEST_CASE("To check if ADC o/p 4095 or greater is read as error by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(4096, &AmpValue) == 1); /* function returns 1 indicating error */
+  REQUIRE(ConvertAdcToAmp(4095, &AmpValue) == ADCToAmpConversionNotSucessful); /* function returns 1 indicating error */
   REQUIRE(AmpValue == 10); /* retains previous value and function doesn't modify the value */
+}
+
+TEST_CASE("To check if ADC o/p 1146 is read as 3A by the converter")
+{
+  REQUIRE(ConvertAdcToAmp(1146, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
+  REQUIRE(AmpValue == 3); 
 }
