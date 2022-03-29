ee98dd2863f8ae345fb9a5a91953ff8c988c38e3
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-29 20:37:06 +0530

    Update ChargingAdcToAmpConverter.h

--- ChargingAdcToAmpConverter.h
+++ ChargingAdcToAmpConverter.h
@@ -7,8 +7,8 @@
 #define MAX_CURRENT_SENSED_BY_ADC             10  /* Amps */
 #define MAX_NUMBER_OF_BITS_SUPPORTED_BY_ADC   12 /* Bits */
 #define MIN_VALUE_READ_BY_ADC                 0
-#define MAX_VALUE_READ_BY_ADC                 4095
-#define ERROR_IN_ADC_READING                  4096
+#define MAX_VALUE_READ_BY_ADC                 4094
+#define ERROR_IN_ADC_READING                  4095
 
 
 int ConvertAdcToAmp(unsigned int ADCValue, unsigned int* CurrentInAmp);
