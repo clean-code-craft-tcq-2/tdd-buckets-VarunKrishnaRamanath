3201180f61ed1011bc8aa2611dd65dca18fe9d6d
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-29 20:20:40 +0530

    Update ChargingAdcToAmpConverter.c

--- ChargingAdcToAmpConverter.c
+++ ChargingAdcToAmpConverter.c
@@ -1,7 +1,10 @@
 #include "ChargingAdcToAmpConverter.h"
 
+#define CONVERT_ADC_TO_AMP(CurrentInAmp)  ((MAX_CURRENT_SENSED_BY_ADC*(*CurrentInAmp))/MAX_VALUE_READ_BY_ADC)
+
 int ConvertAdcToAmp(unsigned int ADCValue, unsigned int* CurrentInAmp)
 {
-  *CurrentInAmp = 0;
+  *CurrentInAmp = CONVERT_ADC_TO_AMP(CurrentInAmp);
+ 
   return ADCToAmpConversionSucessful;
 }
