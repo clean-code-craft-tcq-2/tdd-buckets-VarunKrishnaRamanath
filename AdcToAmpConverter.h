2dad987abd0d459b27632d50c54386e67f5a33d8
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-29 17:11:44 +0530

    Update AdcToAmpConverter.h

--- AdcToAmpConverter.h
+++ AdcToAmpConverter.h
@@ -1,2 +1,8 @@
 #include <stdbool.h>
 #include <string.h>
+
+#define MAX_CURRENT_SENSED_BY_ADC             10  /* Amps */
+#define MAX_NUMBER_OF_BITS_SUPPORTED_BY_ADC   12 /* Bits */
+#define MIN_VALUE_READ_BY_ADC                 0
+#define MAX_VALUE_READ_BY_ADC                 4095
+#define ERROR_IN_ADC_READING                  4096
