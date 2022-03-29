2b29527407c5569ad9a6623c9c2e4609a7488a9b
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-29 22:13:54 +0530

    Update test_ChargingAdcToAmpConverter.cpp

--- test_ChargingAdcToAmpConverter.cpp
+++ test_ChargingAdcToAmpConverter.cpp
@@ -3,6 +3,7 @@
 #include "test/catch.hpp"
 #include <string.h>
 #include "ChargingAdcToAmpConverter.h"
+#include "BatteryChargeAnalyser.h"
 
 int AmpValue = 0;
 TEST_CASE("To check if ADC o/p 0 is read as 0A by the converter")
@@ -38,4 +39,17 @@ TEST_CASE("To supply array of ADC values to a function and get array of CurrentI
   
   REQUIRE(ConvertAdcArrayToAmpArray(AdcArray, CurrentArrayInAmp, sizeOfAdcArray) == 0); /* function returns 0 indicating no error */
   REQUIRE(memcmp(CurrentArrayInAmp, expected_CurrentArrayInAmp, sizeOfAdcArray) == 0); /* converted array matches with expected values */
+}
+
+TEST_CASE("After integration of BatteryChargeAnalyser, pass array of ADC values and expect the ranges as output")
+{
+  int AdcArray[10] = {1146, 1646, 1770, 2500, 2300, 3000, 3400, 3500, 3700, 4000};
+  int CurrentArrayInAmp[10];
+  unsigned int sizeOfAdcArray = sizeof(AdcArray)/sizeof(int);
+  char CurrentRange[1000];
+  
+  /* First convert adc to amp */
+  REQUIRE(ConvertAdcArrayToAmpArray(AdcArray, CurrentArrayInAmp, sizeOfAdcArray) == 0); /* function returns 0 indicating no error */
+  /* Pass the array in amp to range detection function */
+  REQUIRE(strcmp(NumberOfReadingsInRange((unsigned int*)CurrentArrayInAmp, sizeOfAdcArray)), "3-4, 3\n6-10, 7\n") == 0);
 }
