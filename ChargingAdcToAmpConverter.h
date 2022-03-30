87eb215fb40046a49543291fa7d459c3435dbc6a
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-30 13:16:08 +0530

    Update ChargingAdcToAmpConverter.h

--- ChargingAdcToAmpConverter.h
+++ ChargingAdcToAmpConverter.h
@@ -4,12 +4,15 @@
 #define ADCToAmpConversionSucessful      0
 #define ADCToAmpConversionNotSucessful   1
 
-#define MAX_CURRENT_SENSED_BY_ADC             10  /* Amps */
-#define MAX_NUMBER_OF_BITS_SUPPORTED_BY_ADC   12 /* Bits */
-#define MIN_VALUE_READ_BY_ADC                 0
-#define MAX_VALUE_READ_BY_ADC                 4094
-#define ERROR_IN_ADC_READING                  4095
+typedef struct{
+  int MinCurrentSensedByAdc;
+  int MaxCurrentSensedByAdc;
+  int AdcConversionScale;
+  int AdcConversionOffset;
+  int MaxValueReadByAdc;
+}AdcParameters;
 
+extern const AdcParamters AdcParameterStruct;
 
 int ConvertAdcToAmp(unsigned int ADCValue, int* CurrentInAmp);
 int ConvertAdcArrayToAmpArray(int* AdcArray, int* CurrentArrayInAmp, unsigned int sizeOfAdcArray);
