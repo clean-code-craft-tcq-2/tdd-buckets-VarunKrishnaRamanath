1e891a02eb02908de0f68553a69b938144cbd771
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-30 17:55:37 +0530

    Create test_ChargingAndDischargingAdcToAmpConverter.cpp

---
+++ test_ChargingAndDischargingAdcToAmpConverter.cpp
@@ -0,0 +1,43 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include <string.h>
+#include "ChargingAdcToAmpConverter.h"
+#include "BatteryChargeAnalyser.h"
+
+const AdcParameters AdcParameterStruct = {
+  -15,      // MinCurrentSensedByAdc in Amp
+  15,     // MaxCurrentSensedByAdc in Amp
+  30,     // AdcConversionScale -> Since this is dual direction, 2*maxCurrentSensedByAdc is considered as scale
+  -15,      // AdcConversionOffset
+  1022    // MaxValueReadByAdc
+};
+
+int AmpValue = 0;
+TEST_CASE("To check if ADC o/p 0 is read as -15A by the converter")
+{
+  int AdcOutput = 0;
+  int expectedCurrentInAmp = -15;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
+
+TEST_CASE("To check if ADC o/p 1022 is read as 15A by the converter")
+{
+  int AdcOutput = 1022;
+  int expectedCurrentInAmp = 15;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
+
+TEST_CASE("To check if ADC o/p 1023 or greater is read as error by the converter")
+{
+  int AdcOutput = 1023;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
+
+TEST_CASE("To check if ADC o/p 511 is read as 0A by the converter")
+{
+  int AdcOutput = 511;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
+}
