f662f8e26543abf40e841d98d32f361f4522fe08
VarunKrishnaRamanath <8848364+VarunKrishnaRamanath@users.noreply.github.com>
2022-03-30 17:53:22 +0530

    Update test_ChargingAdcToAmpConverter.cpp

--- test_ChargingAdcToAmpConverter.cpp
+++ test_ChargingAdcToAmpConverter.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "test_ADC.h"
 #include <string.h>
 #include "ChargingAdcToAmpConverter.h"
 #include "BatteryChargeAnalyser.h"
@@ -13,29 +14,33 @@ const AdcParameters AdcParameterStruct = {
   4094    // MaxValueReadByAdc
 };
 
-int AmpValue = 0;
+
 TEST_CASE("To check if ADC o/p 0 is read as 0A by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(0, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
-  REQUIRE(AmpValue == 0);
+  int AdcOutput = 0;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
 }
 
 TEST_CASE("To check if ADC o/p 4094 is read as 10A by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(4094, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
-  REQUIRE(AmpValue == 10);
+  int AdcOutput = 4094;
+  int expectedCurrentInAmp = 10;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
 }
 
 TEST_CASE("To check if ADC o/p 4095 or greater is read as error by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(4095, &AmpValue) == ADCToAmpConversionNotSucessful); /* function returns 1 indicating error */
-  REQUIRE(AmpValue == 10); /* retains previous value and function doesn't modify the value */
+  int AdcOutput = 4095;
+  int expectedCurrentInAmp = 0;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
 }
 
 TEST_CASE("To check if ADC o/p 1146 is read as 3A by the converter")
 {
-  REQUIRE(ConvertAdcToAmp(1146, &AmpValue) == ADCToAmpConversionSucessful); /* function returns 0 indicating no error */
-  REQUIRE(AmpValue == 3); 
+  int AdcOutput = 1146;
+  int expectedCurrentInAmp = 3;
+  validateAdcConverter_NoError(AdcOutput, expectedCurrentInAmp);
 }
 
 TEST_CASE("To supply array of ADC values to a function and get array of CurrentInAmp as output")
@@ -45,8 +50,8 @@ TEST_CASE("To supply array of ADC values to a function and get array of CurrentI
   int expected_CurrentArrayInAmp[10] = {3, 4, 4, 6, 6, 7, 8, 9, 9, 10};
   unsigned int sizeOfAdcArray = sizeof(AdcArray)/sizeof(int);
   
-  REQUIRE(ConvertAdcArrayToAmpArray(AdcArray, CurrentArrayInAmp, sizeOfAdcArray) == 0); /* function returns 0 indicating no error */
-  REQUIRE(memcmp(CurrentArrayInAmp, expected_CurrentArrayInAmp, sizeOfAdcArray) == 0); /* converted array matches with expected values */
+  CheckCurrentArrayWithAdcInput(AdcArray, expected_CurrentArrayInAmp, sizeOfAdcArray);
+
 }
 
 TEST_CASE("After integration of BatteryChargeAnalyser, pass array of ADC values and expect the ranges as output")
@@ -54,10 +59,6 @@ TEST_CASE("After integration of BatteryChargeAnalyser, pass array of ADC values 
   int AdcArray[10] = {1146, 1646, 1770, 2500, 2300, 3000, 3400, 3500, 3700, 4000};
   int CurrentArrayInAmp[10];
   unsigned int sizeOfAdcArray = sizeof(AdcArray)/sizeof(int);
-  char CurrentRange[1000];
   
-  /* First convert adc to amp */
-  REQUIRE(ConvertAdcArrayToAmpArray(AdcArray, CurrentArrayInAmp, sizeOfAdcArray) == 0); /* function returns 0 indicating no error */
-  /* Pass the array in amp to range detection function */
-  REQUIRE(strcmp(NumberOfReadingsInRange((unsigned int*)CurrentArrayInAmp, sizeOfAdcArray), "3-4, 3\n6-10, 7\n") == 0);
+  CheckRangesWithAdcInput(AdcArray,"3-4, 3\n6-10, 7\n",sizeOfAdcArray) == 0);
 }
